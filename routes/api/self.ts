1585fab3d7098d2a366d503c28e213dd5b28ea9c
rroyalty <rroyalty@gmail.com>
2021-07-22 11:17:51 -0400

    test

--- routes/api/self.ts
+++ routes/api/self.ts
@@ -9,7 +9,6 @@ const vars: SnoowrapOptions = {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: process.env.ACCESS_TOKEN
 };
 
 // TEST
