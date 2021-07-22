9fb81c50d22ccdaea10f10fa5c3802478ec9efb1
rroyalty <rroyalty@gmail.com>
2021-07-22 11:02:05 -0400

    snoowrap test

--- routes/api/self.ts
+++ routes/api/self.ts
@@ -15,7 +15,7 @@ const vars: SnoowrapOptions = {
 router.get('/', async (req: Request, res: Response) => {
     try {
         const snoo = await new snoowrap(vars);
-        const user = snoo.getUser('rroyaltywebdev').getSubmissions();
+        const user = await snoo.getUser('rroyaltywebdev').getSubmissions();
         res.status(200).json(user)
     } catch (err) {
         console.log(err)
